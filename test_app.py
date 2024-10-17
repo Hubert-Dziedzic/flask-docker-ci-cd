a47c3530358a3553d910b0c7b3e983d3f9cbf95a
root <root@localhost.localdomain>
2024-10-17 20:38:59 +0200

    Sloving an error in the code

--- test_app.py
+++ test_app.py
@@ -9,9 +9,9 @@ class BasicTests(unittest.TestCase):
 		self.app.testing = True
 	
 	def test_main_page(self):
-	#Send Get on '/'
-	response = self.app.get('/')
-	self.assertEqual(response.status_code, 200)
-	self.assertEqual(response.data, b'Hello, World!')
+	    #Send Get on '/'
+	    response = self.app.get('/')
+	    self.assertEqual(response.status_code, 200)
+	    self.assertEqual(response.data, b'Hello, World!')
 	
 if __name__ == "__main__":
